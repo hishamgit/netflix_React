d40ebe693be1a8cda8359d15b0271055c2c1f850
hishamgit <mohammedhishamk81@gmail.com>
2023-09-04 01:08:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import './App.css'
+import Banner from "./components/Banner/Banner";
+import RowPost from "./components/RowPost/RowPost";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Banner/>
+      <RowPost/>
+    </div>
+  );
+}
+
+export default App;
