d7018da2d78f32159f9a8081aa98fbe8648b0eec
hishamgit <mohammedhishamk81@gmail.com>
2023-09-06 17:03:06 +0530

    trailer option,design change

--- src/components/RowPost/RowPost.js
+++ src/components/RowPost/RowPost.js
@@ -1,10 +1,12 @@
 import React,{useEffect,useState} from 'react'
 import './RowPost.css'
 import axios from '../../axios'
-import {imageUrl} from '../../constants/constants'
+import {imageUrl,API_KEY} from '../../constants/constants'
+import Youtube from 'react-youtube'
 
 function RowPost(props) {
     const [movies,setMovies]=useState([])
+    const [urlId,setUrlId]=useState()
     useEffect(()=>{
         axios.get(props.url).then((response)=>{
             // console.log(response.data.results[0])
@@ -13,16 +15,36 @@ function RowPost(props) {
             // alert('Network Error')
         })
     },[])
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+    }
+    const handleMovie=(id)=>{
+        //  console.log(id)
+         axios.get(`movie/${id}/videos?language=en-US&api_key=${API_KEY}`).then((response)=>{
+            if(response.data.results.length!==0){
+                setUrlId(response.data.results[0])
+            }else{
+                console.log('Array empty')
+            }
+         })
+
+    }
     return (
         <div className='row'>
             <h2>{props.title}</h2>
             <div className="posters">
                 {
                     movies.map((obj)=>
-                        <img className={props.isSmall ?'smallPoster':'poster'} src={`${imageUrl+obj.backdrop_path}` } alt="poster" /> //src in {} to include ``
+                        <img onClick={()=>{handleMovie(obj.id)}} className={props.isSmall ?'smallPoster':'poster'} src={`${imageUrl+obj.backdrop_path}` } alt="poster" /> //src in {} to include ``
                     )
                 }
             </div>
+            {urlId && <Youtube opts={opts} videoId={urlId.key}/>}
         </div>
     )
 }
